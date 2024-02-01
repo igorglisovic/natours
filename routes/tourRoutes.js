8c9d60109abd5645a1d08a43f1342bb36ffc26c9
Gliso <igorglisovic2@gmail.com>
2024-02-01 04:55:02 +0100

    Aggregation operation

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,10 +6,14 @@ const {
   updateTour,
   deleteTour,
   getTopCheapTours,
+  getTourStats,
+  getMonthlyPlan,
 } = require('../controllers/tourController')
 
 const router = express.Router()
 
+router.route('/tour-stats').get(getTourStats)
+router.route('/monthly-plan/:year').get(getMonthlyPlan)
 router.route('/top-5-cheap').get(getTopCheapTours, getAllTours)
 router.route('/').get(getAllTours).post(createTour)
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour)
